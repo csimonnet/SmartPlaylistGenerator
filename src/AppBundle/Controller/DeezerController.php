048568562a20e8ad6b41ca65c347bdbddcf192b6
csimonnet <charlotte.simonnet@gmail.com>
2017-08-24 21:38:33 +0200

    Adding Deezer parameters and Deezer Controller

---
+++ src/AppBundle/Controller/DeezerController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class DeezerController
+{
+    /**
+     * @Route("/deezer/connect")
+     */
+    public function connectAction()
+    {
+    }
+}
