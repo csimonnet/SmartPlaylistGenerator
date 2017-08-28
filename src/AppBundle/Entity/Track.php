f89aacc52974a4fa4a93a6df887d5c71fa9a11e5
csimonnet <charlotte.simonnet@gmail.com>
2017-08-28 13:44:19 +0200

    Completing track object

--- src/AppBundle/Entity/Track.php
+++ src/AppBundle/Entity/Track.php
@@ -10,4 +10,37 @@ class Track
 
     protected $album;
 
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setDeezerId($deezerId)
+    {
+        $this->deezerId = $deezerId;
+        return $this;
+    }
+
+    public function getDeezerId()
+    {
+        return $this->deezerId;
+    }
+
+    public function setAlbum(Album $album)
+    {
+        $this->album = $album;
+        return $this;
+    }
+
+    public function getAlbum()
+    {
+        return $this->album;
+    }
+
 }
